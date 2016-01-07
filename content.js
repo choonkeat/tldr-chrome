5c24ca773122d62f179c303bb522bff9dd05f1cf
choonkeat <choonkeat@gmail.com>
2016-01-07 23:24:24 +0800

    Using inserted "<style>" to simplify js logic

--- content.js
+++ content.js
@@ -1,25 +1,40 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message === "clicked_browser_action" ) {
-      $('p').each(function() {
-        var that = this;
-        var range = new Range();
-        range.setStart(that, 0);
+      if (! $('style[data-topicsentence-style')[0]) {
+        var css = [
+              ".topicsentence-parent { visibility: hidden !important; }",
+              ".topicsentence-parent .topicsentence { visibility: visible !important; }",
+            ].join("\n"),
+            head = document.head || document.body,
+            style = document.createElement('style');
+
+        style.type = 'text/css';
+        $(style).attr('data-topicsentence-style', true);
+        style.appendChild(document.createTextNode(css));
+        head.appendChild(style);
+      }
+
+      $('p[data-topicsentence-parent]').each(function(index, that) {
+        $(that).toggleClass('topicsentence-parent');
+      });
+      $('p:not([data-topicsentence-parent])').each(function(index, that) {
         $.each(that.childNodes, function() {
           if (this.nodeName == '#text') {
-            var match = this.nodeValue && this.nodeValue.match(/\.\s/);
-            if ($(that).data('topicsentence-parent')) {
-              $(that).find('[data-topicsentence-show]').css({ visibility: '' }).find('*').unwrap();
-              $(that).data('topicsentence-parent', false).css({ visibility: '' });
-              return false; // break $.each
-            } else if (match) {
-              range.setEnd(this, match.index);
+            var match = this.nodeValue && this.nodeValue.match(
+              /[^\.]\w\.\s/
+              // do not treat acronym periods as sentence period
+
+            );
+            if (match) {
+              var range = new Range();
+              range.setStart(that, 0);
+              range.setEnd(this, match.index + 3); // skip past to include the "." in topic sentence
               var newNode = document.createElement("span");
               range.surroundContents(newNode);
-              $(newNode).attr('data-topicsentence-show', true);
-              $(that).data('topicsentence-parent', true).css({ visibility: 'hidden' });
-              $('[data-topicsentence-show], [data-topicsentence-show] *').css({ visibility: 'visible' });
-              return false; // break $.each
+              $(newNode).addClass('topicsentence');
+              $(that).addClass('topicsentence-parent').attr('data-topicsentence-parent', 'on');
+              return false; // break $.each childNodes
             }
           }
         });
